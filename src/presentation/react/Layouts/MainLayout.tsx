b8a27516fd5a3baea956fa86ac0724b23ae7e0eb
Joiss Ventura Rivera <2389790@gmail.com>
2025-05-20 00:19:36 -0500

    Naming al contenido de la aplicacion

--- src/presentation/react/Layouts/MainLayout.tsx
+++ src/presentation/react/Layouts/MainLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 
 export const MainLayout = () => {
     return (
-        <div>
+        <div className='app'>
             <Outlet />
         </div>
     )
